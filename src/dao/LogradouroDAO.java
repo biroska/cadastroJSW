aea2229b3a2be7ceadb4dd52764bf7a51c3bee1e
biroska <biroska@gmail.com>
2014-04-24 12:06:09 -0300

    correção do LogradouroDAO

--- src/dao/LogradouroDAO.java
+++ src/dao/LogradouroDAO.java
@@ -8,17 +8,17 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Repository;
 
 import entidade.Logradouro;
-import entidade.LogradouroEntity;
+import entidade.Logradouro;
 
 @Repository("logradouroDAO")
 @Scope( BeanDefinition.SCOPE_PROTOTYPE )
 public class LogradouroDAO extends BaseDAO {
 
-	public List<LogradouroEntity> carregarTodosLogradouros(){
+	public List<Logradouro> carregarTodosLogradouros(){
 
 		Criteria criteria = getSessionFactory().openSession().createCriteria(Logradouro.class);
 		
-		List<LogradouroEntity> logradouros = (List<LogradouroEntity>) criteria.list();
+		List<Logradouro> logradouros = (List<Logradouro>) criteria.list();
 		
 		getSessionFactory().close();
 		
